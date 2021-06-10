d8d2c0a2c9374dac6aa345aa109c4663965feab0
amboxer21 <amboxer21@gmail.com>
2021-06-10 18:08:04 -0400

    Removed queue support and added optparser support

--- realtime_common_object_detection.py
+++ realtime_common_object_detection.py
@@ -2,11 +2,12 @@ import os
 import cv2
 import time
 import threading
-import multiprocessing
 import concurrent.futures
 
 import numpy as np
 import cvlib as cv
+
+from optparse import OptionParser
 from cvlib.object_detection import draw_bbox
 
 class RTCObjectDetection(object):
@@ -16,25 +17,32 @@ class RTCObjectDetection(object):
 
     bbox, label, conf = str(), str(), str()
 
-    def __init__(self,filename='/home/anthony/video.avi'):
+    def __init__(self,config_dict={}):
 
-        self.capture = cv2.VideoCapture('/dev/cam0')
+        self.camera   = config_dict['camera']
+        self.object   = config_dict['object']
+        self.logfile  = config_dict['logfile']
+        self.verbose  = config_dict['verbose']
+        self.filename = config_dict['filename']
+
+        self.capture = cv2.VideoCapture(self.camera)
         self.capture.set(3,1080)
         self.capture.set(4,1080)
 
         self.fps = self.capture.get(cv2.CAP_PROP_FPS)
         
         self.video_writer = cv2.VideoWriter(
-            filename, cv2.VideoWriter_fourcc(*'MJPG'), self.fps,
+            self.filename, cv2.VideoWriter_fourcc(*'MJPG'), self.fps,
             (int(self.capture.get(3)), int(self.capture.get(4)))
         )
         
-        print('[INFO] (RTCObjectDetection.__init__) - fps => '+str(self.fps))
+        if self.verbose:
+            print('[INFO] (RTCObjectDetection.__init__) - fps => '+str(self.fps))
 
     @staticmethod
-    def detect_common_objects(frame,queue={}):
+    def detect_common_objects(frame, worker_count=4):
         try:
-            with concurrent.futures.ThreadPoolExecutor(max_workers = 4) as executor:
+            with concurrent.futures.ThreadPoolExecutor(max_workers = worker_count) as executor:
                 thread = executor.submit(cv.detect_common_objects, frame)
                 RTCObjectDetection.bbox, RTCObjectDetection.label, RTCObjectDetection.conf = thread.result()
         except Exception as eStartThread:
@@ -52,7 +60,7 @@ class RTCObjectDetection(object):
     def init_o_bbox(self,o_bbox):
         pass
 
-    def main(self, queue):
+    def main(self):
 
         # Initialize the frame count tracker to 0
         counter = 0
@@ -73,9 +81,15 @@ class RTCObjectDetection(object):
             if RTCObjectDetection.label:
 
                 frame = draw_bbox(frame, RTCObjectDetection.bbox, RTCObjectDetection.label, RTCObjectDetection.conf)
-                index = self.find_index_of_label(RTCObjectDetection.label,'bottle')
+                index = self.find_index_of_label(RTCObjectDetection.label,self.object)
 
-                if 'bottle' in RTCObjectDetection.label:
+                if self.verbose:
+                    print('[INFO] (RTCObjectDetection.main) - All labels: '+str(RTCObjectDetection.label))
+
+                if self.object in RTCObjectDetection.label:
+
+                    if self.verbose:
+                        print('[INFO] (RTCObjectDetection.main) - Object label: '+str(RTCObjectDetection.label))
 
                     # Initialize original bbox if no value has been assigned yet
                     if o_bbox is None or not o_bbox:
@@ -91,13 +105,13 @@ class RTCObjectDetection(object):
 
                     try:
                         if (abs(n1-n2) > 50).any():
-                            print('Cell phone movement detected!')
+                            print('[INFO] (RTCObjectDetection.main) - '+self.object+' movement detected!')
                             print('[INFO] (RTCObjectDetection.main) - o_bbox numpy(n1) array => '+str(n1))
                             print('[INFO] (RTCObjectDetection.main) - bbox numpy(n2) array   => '+str(n2))
                             # If movement of desired(specified) object is detected re-init o_bbox's values
                             o_bbox = RTCObjectDetection.bbox[index]
                     except Exception as exception:
-                        print('Exception exception => '+str(exception))
+                        print('[ERROR] (RTCObjectDetection.main) - Exception exception => '+str(exception))
                         pass
 
                 RTCObjectDetection.label = None
@@ -111,9 +125,38 @@ class RTCObjectDetection(object):
 
 
 if __name__ == '__main__':
-    queue = multiprocessing.Queue(maxsize=3)
 
-    rtc_object_detection = RTCObjectDetection()
-    rtc_object_detection.main(queue)
+    parser = OptionParser()
+
+    parser.add_option('-c', '--camera-location',
+        dest='camera', default='/dev/cam0',
+        help='This is the default camera path.')
+
+    parser.add_option('-o', '--object',
+        dest='object', default='bottle',
+        help='This is the default object we will be tracking.')
+
+    parser.add_option('-v', '--verbose',
+        dest='verbose', action='store_true', default=False,
+        help="Turns on verbose output. This is turned off by default.")
+
+    parser.add_option('-l', '--log-file',
+        dest='logfile', default='/var/log/opencv-tracker.log',
+        help='Log file defaults to /var/log/opencv-tracker.log.')
+
+    parser.add_option('-f', '--file-name',
+        dest='filename', default='/home/anthony/video.avi',
+        help='Filename defaults to /home/anthony/video.avi.')
+
+    (options, args) = parser.parse_args()
+
+    config_dict = {
+        'camera': options.camera,
+        'object': options.object, 'verbose': options.verbose,
+        'logfile': options.logfile, 'filename': options.filename,
+    }
+
+    rtc_object_detection = RTCObjectDetection(config_dict)
+    rtc_object_detection.main()
 
     cv2.destroyAllWindows()
